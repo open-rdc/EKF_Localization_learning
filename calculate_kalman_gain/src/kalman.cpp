211be4a6cb108afc67f2392d8a94811ea45cfeeb
Kazuya.N <aibotaro@gmail.com>
2015-04-21 01:13:06 +0900

    Create calculate kalman gain class and callback functions for subscriber

--- calculate_kalman_gain/src/kalman.cpp
+++ calculate_kalman_gain/src/kalman.cpp
@@ -12,38 +12,52 @@
 #include <complex>
 
 using namespace Eigen;
-ros::Publisher K_pub;
-ros::Publisher H_pub;
 
-void kalman (const std_msgs::Float64MultiArray::ConstPtr& Sigma_t, const std_msgs::Float64MultiArray::ConstPtr& mu_t, const std_msgs::Float64MultiArray::ConstPtr& m_t)
-{
-    //行列とベクトルの宣言
-    MatrixXf Sigma(3,2);
+class Kalman{
+public:
+  Kalman(){
+    Sigma_t_sub = nh.subscribe("/Sigma_t",10, &Kalman::sigma_tCallBack, this);
+    mu_t_sub = nh.subscribe("/mu_t",10, &Kalman::mu_tCallBack, this);
+    m_t_sub = nh.subscribe("/m_t",10, &Kalman::m_tCallBack, this);
+
+    K_pub = nh.advertise<std_msgs::Float64MultiArray>("/K", 1000);
+    H_pub = nh.advertise<std_msgs::Float64MultiArray>("/H", 1000);
+
+    ros::Rate loop_rate(10);
+  }
+
+  void sigma_tCallBack(const std_msgs::Float64MultiArray::ConstPtr& Sigma_t){
+    for(int i=0; i<3; i++){
+      for(int j=0; j<3; j++){
+        Sigma(i,j) = Sigma_t->data[i+j];
+      }
+    }
+  }
+
+  void mu_tCallBack(const std_msgs::Float64MultiArray::ConstPtr& mu_t){
+    for(int i=0; i<3; i++){
+      mu(i) = mu_t->data[i];
+    }
+  }
+
+  void m_tCallBack(const std_msgs::Float64MultiArray::ConstPtr& m_t){
+    for(int i=0; i<3; i++){
+      m(i) = m_t->data[i];
+    }
+  }
+
+  void calc(){
     MatrixXf H = MatrixXf::Zero(3, 3);
     MatrixXf Q = MatrixXf::Identity(3, 3);
     MatrixXf S = MatrixXf::Zero(3, 3);
     MatrixXf K = MatrixXf::Zero(3, 3);
-    Vector3f mu(0, 0, 0);
-    Vector3f m(0, 0, 0);
     float q=0;
     int i,j;
     std_msgs::Float64MultiArray K_p;
     std_msgs::Float64MultiArray H_p;
 
-
-    for(i = 0; i < 3; ++i)
-    {
-        mu(i) = mu_t -> data[i];
-        m(i) = m_t -> data[i];
-        for(j = 0; j < 3; ++j)
-        {
-            //Σへの代入
-            Sigma(i,j) = Sigma_t -> data[i+j];
-        }
-    }
-
     q = (m(0)-mu(0))*(m(0)-mu(0)) + (m(1)-mu(1))*(m(1)-mu(1));
-   // z_h << sqrt(q), atan2(m(1)-mu(1),m(0)-mu(0))-mu(2), m(2);
+    // z_h << sqrt(q), atan2(m(1)-mu(1),m(0)-mu(0))-mu(2), m(2);
     //Hの計算
     H <<
     -(m(0)-mu(0))/sqrt(q), -(m(1)-mu(1))/sqrt(q), 0,
@@ -64,28 +78,36 @@ void kalman (const std_msgs::Float64MultiArray::ConstPtr& Sigma_t, const std_msg
 
     K_pub.publish (K_p);
     H_pub.publish (H_p);
+    //ToDo
+  }
 
-}
+  void run(){
+    while(ros::ok()){
+      calc();
+      ros::spinOnce();
+    }
+  }
 
+private:
+  ros::Subscriber Sigma_t_sub;
+  ros::Subscriber mu_t_sub;
+  ros::Subscriber m_t_sub;
+
+  ros::Publisher K_pub;
+  ros::Publisher H_pub;
+
+  ros::NodeHandle nh;
+
+  MatrixXf Sigma = MatrixXf::Zero(3,2);
+  Vector3f mu = Vector3f::Zero();
+  Vector3f m = Vector3f::Zero();
+};
 
 int main(int argc, char **argv)
 {
- //   typedef message_filters::sync_policies::ApproximateTime<std_msgs::Float64MultiArray::ConstPtr,std_msgs::Float64MultiArray::ConstPtr,std_msgs::Float64MultiArray::ConstPtr> SyncPolicy;
     ros::init(argc, argv, "kalman");
-    ros::NodeHandle n;
+    Kalman kalman;
+    kalman.run();
 
-
-   //message_filters::Subscriber<std_msgs::Float64MultiArray::ConstPtr> Sigma_t (n, "/Sigma_t", 10);
-   //message_filters::Subscriber<std_msgs::Float64MultiArray::ConstPtr> mu_t (n, "/mu_t", 10);
-   //message_filters::Subscriber<std_msgs::Float64MultiArray::ConstPtr> m_t (n, "/m_t", 10);
-    
-   //message_filters::Synchronizer<SyncPolicy> sync_(SyncPolicy(10),Sigma_t,mu_t,m_t);
-   //sync_.registerCallback(boost::bind(&kalman, _1, _2, _3));
-
-    K_pub = n.advertise<std_msgs::Float64MultiArray>("/K", 1000);
-    H_pub = n.advertise<std_msgs::Float64MultiArray>("/H", 1000);
-
-    ros::Rate loop_rate(10);
-
-    ros::spin();
+    return 0;
 }
